2ef84fd41e8b91b843f40668570ec6bef2ad98de
JASaddington <jasaddington@gmail.com>
2023-05-14 14:37:48 -0500

    building game.js, getting placeholders on screen

--- public/js/game.js
+++ public/js/game.js
@@ -4,7 +4,8 @@ window.addEventListener('load', function(){
   const ctx = canvas.getContext('2d');
   canvas.width = 750;
   canvas.height = 500;
-
+  // classes for encapsulation and inheritance.  
+  // !REFACTOR CLASSES LATER
   class Input{
 
   }
@@ -15,6 +16,24 @@ window.addEventListener('load', function(){
 
   }
   class Player{
+    // game as argument to give access to properties of Game class
+    constructor(game){
+      this.game = game;
+      this.width= 120;
+      this.height = 190;
+      // player starting position
+      this.x = 20;
+      this.y = 100;
+      // player starting speed
+      this.speedY = 0;
+    }
+    update(){
+
+    }
+    draw(context){
+      // arguments: location and size measurements of player
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
 
   }
   class Enemy{
@@ -30,7 +49,28 @@ window.addEventListener('load', function(){
 
   }
   class Game{
+    // width and height of game matches size of canvas el
+    constructor(width, height){
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
     
-  }
+    }
+    update(){
+      this.player.update();
+    }
+    draw(context){
+      this.player.draw(context);
+    }
 
+  }
+  // make the game and animate it on a continuous loop 
+  const game = new Game(canvas.width, canvas.height);
+  function animate(){
+    game.update();
+    game.draw(ctx);
+    // update animation before next refresh, loop.
+    requestAnimationFrame(animate);
+  }
+  animate();
 })
